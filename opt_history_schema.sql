62ddfe18880c71aa43710f2e9d0f6815ccff8ac4
eegisas <86287431+eegisas@users.noreply.github.com>
2025-03-05 08:18:55 +0200

    Update opt_history_schema.sql

--- opt_history_schema.sql
+++ opt_history_schema.sql
@@ -7,4 +7,20 @@ CREATE TABLE `opt_history` (
   `low` float NOT NULL,
   `close` float NOT NULL,
   `barSizeSetting` varchar(16) NOT NULL,
-  `whatTo
+  `whatToShow` varchar(16) NOT NULL,
+  `symbol` varchar(32) NOT NULL,
+  `conFilter` varchar(32) NOT NULL,
+  `wap` float DEFAULT NULL,
+  `volume` float DEFAULT NULL,
+  `id` int DEFAULT NULL,
+  `intConFilter` int GENERATED ALWAYS AS (cast(left(`conFilter`,8) as unsigned)) VIRTUAL,
+  `dateLastRunID` datetime DEFAULT NULL,
+  `time_old` varchar(32) DEFAULT NULL,
+  `rowNo` varchar(45) DEFAULT '0',
+  PRIMARY KEY (`conId`,`time`,`barSizeSetting`,`whatToShow`),
+  KEY `IconFilter` (`symbol`,`conFilter`(8),`dateLastRunID`,`barSizeSetting`,`whatToShow`,`conId`,`date`),
+  KEY `barSize` (`barSizeSetting`(8)),
+  KEY `date` (`date`),
+  KEY `idIDX` (`id`),
+  KEY `NumConFilter` (`symbol`,`intConFilter`,`dateLastRunID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
